87f10509b90376ab6594e1c096c77e8528c86b12
tur-ium <tur-ium@users.noreply.github.com>
2018-02-13 19:30:19 +0000

    Add files via upload

--- artur-12-2-18-calculating-clustering-coeffs-on-timeslices.py
+++ artur-12-2-18-calculating-clustering-coeffs-on-timeslices.py
@@ -4,6 +4,15 @@ Created on Mon Feb 12 22:22:30 2018
 
 @author: admin
 
+TODO: Uncertainty
+    Sampling
+    Rerun
+TODO: Null models - significance
+
+TODO: number of users increases over time?
+TODO: Av. degree
+TOD0: Read Networkx code
+
 Note that this will not work if there is a user called 'NaN'
 """
 import Timeslice as ts
@@ -15,12 +24,12 @@ import matplotlib.pyplot as plt
 dataPath = "Data/WeightedNetwork"
 noMonths = 3
 startDate = ts.EPOCH_MIN
-endDate = ts.TIME_DICT["2009"]["JUNE"]
+endDate = ts.TIME_DICT["2009"]["JUN"]
 
 runtimes = list()
 timesliceNames = list()
 avgClusteringCoeffs = list()
-
+degreeList = list()
 
 
 def computeClusteringCoefficients(Graph, isWeighted=False):
@@ -43,12 +52,12 @@ counter = 0
 Graph = nx.Graph()
 Graph.add_edge('NaN','NaN',weight=1)  #Add a weighted edge in order to ensure that weighted addition can occur 
 
-for YEAR in ["2007","2008","2009"]:
+for YEAR in ["2007","2008"]:
     for MONTH in ts.MONTHS:
         counter += 1
         
         startTime = time.time()
-        if(ts.TIME_DICT[YEAR][MONTH] < startDate or ts.TIME_DICT[YEAR][MONTH] > endDate):
+        if(ts.TIME_DICT[YEAR][MONTH] < startDate or ts.TIME_DICT[YEAR][MONTH] > endDate-1):
             continue
         print('{} {}'.format(MONTH, YEAR))
         
@@ -63,6 +72,8 @@ for YEAR in ["2007","2008","2009"]:
         print('CALCULATING CLUSTERING COEFFICIENTS')
         if counter % noMonths == 0:
             timesliceNames.append('{} {}'.format(MONTH, YEAR))   #Record name
+            degree = sum(dict(nx.degree(Graph)).values())/len(Graph.nodes)
+            degreeList.append(degree)
             avgClustering, clusteringDict = computeClusteringCoefficients(Graph)
             avgClusteringCoeffs.append(avgClustering)
             print("Average Clustering - {} months up to and including {} - {}: {}".format(noMonths, YEAR, MONTH, avgClustering))
@@ -73,8 +84,16 @@ for YEAR in ["2007","2008","2009"]:
         runtimes.append(endTime-startTime)
         print('RUNTIME: {}'.format(runtimes[-1]))
 #PLOT AVG CLUSTERING COEFF OVER TIME
-plt.figure(figsize=(10,5))
-plt.title('Average clustering coefficients')
-plt.ylabel('Av. clustering coefficient')
-plt.xlabel('Timeslice')
-plt.bar(timesliceNames,avgClusteringCoeffs)
+fig = plt.figure(figsize=(10,3))
+ax = fig.add_subplot(311)
+ax1 = fig.add_subplot(313)
+ax.set_title('Average clustering coefficients')
+ax.set_ylabel('Av. clustering coefficient')
+ax.set_xlabel('Timeslice')
+ax1.set_title('Average degree')
+ax1.set_ylabel('Av. degree')
+ax1.set_xlabel('Timeslice')
+ax.plot(range(len(timesliceNames)),avgClusteringCoeffs,'g-')
+ax1.plot(range(len(timesliceNames)),degreeList,'b--')
+ax.set_xticklabels(timesliceNames)
+ax1.set_xticklabels(timesliceNames)
